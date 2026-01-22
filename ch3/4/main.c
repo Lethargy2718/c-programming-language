f4ea3a560fa2d37f4ea67306e6e9f445f21b1723
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2026-01-22 19:27:44 +0200

    fix(ch3): fix exercise 4's problem statement

--- ch3/4/main.c
+++ ch3/4/main.c
@@ -1,8 +1,7 @@
 /*
-Exercise 3-3. Write a function expand(s1,s2) that expands shorthand notations like a-z in
-the string s1 into the equivalent complete list abc...xyz in s2. Allow for letters of either
-case and digits, and be prepared to handle cases like a-b-c and a-z0-9 and -a-z. Arrange
-that a leading or trailing - is taken literally.
+Exercise 3-4. In a two's complement number representation, our version of itoa does not
+handle the largest negative number, that is, the value of n equal to -(2wordsize-1). Explain why
+not. Modify it to print that value correctly, regardless of the machine on which it runs.
 */
 
 #include <stdio.h>
