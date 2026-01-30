9ce0ddbd0ad8119cf947bf51106450e822c7d621
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2026-01-30 17:23:53 +0200

    feat(ch4): add exercise 1

---
+++ ch4/1/main.c
@@ -0,0 +1,32 @@
+/*
+Exercise 4-1. Write the function strindex(s,t) which returns the position of the rightmost
+occurrence of t in s, or -1 if there is none.
+*/
+
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int strindex(char s[], char t[]);
+
+int main(void)
+{
+    char t[] = "ould";
+    char s[] = "aould hello bould hello could hello douldd"; // last "ould" at 37
+    printf("%i\n", strindex(s, t));
+}
+
+int strindex(char s[], char t[])
+{
+    int i, j, k, last = -1;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
+            ;
+        if (k > 0 && t[k] == '\0')
+            last = i;
+    }
+
+    return last;
+}
